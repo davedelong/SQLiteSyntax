4e6155e75350656c5aad7e52b0e47e86e5352879
Dave DeLong <davedelong@me.com>
2025-04-27 15:47:41 -0500

    Make List more fluent

--- Sources/SQLiteSyntax/List.swift
+++ Sources/SQLiteSyntax/List.swift
@@ -7,18 +7,29 @@
 
 import Foundation
 
-public struct List<T: Syntax>: Syntax, ExpressibleByArrayLiteral {
+public struct List<Element: Syntax>: Syntax, ExpressibleByArrayLiteral {
     
-    public var items: Array<T>
+    
+    public var items: Array<Element>
     public var delimiter = ","
     public var includeLeadingDelimeter = false
     
-    public init(items: Array<T>) {
-        self.items = items
+    public init() {
+        self.items = []
     }
     
-    public init(arrayLiteral elements: T...) {
-        self.init(items: elements)
+    public init(items: Array<Element>, delimeter: String, includeLeadingDelimeter: Bool) {
+        self.items = items
+        self.delimiter = delimeter
+        self.includeLeadingDelimeter = includeLeadingDelimeter
+    }
+    
+    public init<S>(_ elements: S) where S : Sequence, Element == S.Element {
+        self.items = Array(elements)
+    }
+    
+    public init(arrayLiteral elements: Element...) {
+        self.init(elements)
     }
     
     public func validate() throws(SyntaxError) {
@@ -29,8 +40,41 @@ public struct List<T: Syntax>: Syntax, ExpressibleByArrayLiteral {
         if includeLeadingDelimeter { builder.add(delimiter) }
         try builder.addList(items, delimiter: delimiter)
     }
+}
+
+/*
+ List conforms to various collection protocols to make interacting with it a bit easier
+ 
+ Instead of having to do:
+ statement.columns.items.append(...)
+ 
+ You can do:
+ statement.columns.append(...)
+ */
+extension List: RangeReplaceableCollection, BidirectionalCollection, MutableCollection, RandomAccessCollection {
     
-    public mutating func append(_ item: T) {
-        self.items.append(item)
+    public typealias SubSequence = List<Element>
+    public typealias Index = Array<Element>.Index
+    
+    public var startIndex: Array<Element>.Index { items.startIndex }
+    public var endIndex: Array<Element>.Index { items.endIndex }
+    public var count: Int { items.count }
+    
+    public func index(before i: Index) -> Index { items.index(before: i) }
+    public func index(after i: Index) -> Index { items.index(after: i) }
+    
+    public subscript(position: Index) -> Element {
+        get { items[position] }
+        set { items[position] = newValue }
+    }
+    public subscript(bounds: Range<Index>) -> List<Element> {
+        get { List(items: Array(items[bounds]),
+                   delimeter: delimiter,
+                   includeLeadingDelimeter: includeLeadingDelimeter) }
+        set { items[bounds] = newValue.items[...] }
+    }
+    
+    public mutating func replaceSubrange<C>(_ subrange: Range<Index>, with newElements: C) where C : Collection, Element == C.Element {
+        items.replaceSubrange(subrange, with: newElements)
     }
 }
