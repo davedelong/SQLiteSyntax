4de56b84b26f374083e5b4ad91bbd808b467ff00
Dave DeLong <davedelong@me.com>
2025-04-29 10:17:46 -0500

    column convenience

---
+++ Sources/SQLiteSyntax/Convenience/Columns.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  SQLiteSyntax
+//
+//  Created by Dave DeLong on 4/29/25.
+//
+
+import Foundation
+
+extension ColumnConstraint {
+    
+    public var notNull: Self {
+        return .init(name: nil, constraint: .notNull(.none))
+    }
+    
+    public var unique: Self {
+        return .init(name: nil, constraint: .unique(.none))
+    }
+    
+}
